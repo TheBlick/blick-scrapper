193fd4b289c7971ed40a58b671989cb01c4fe6b7
gabryuri <gyribeiro2014@gmail.com>
2024-05-07 08:58:00 -0300

    feat: add google interactor

---
+++ src/search_engines/google/google.py
@@ -0,0 +1,34 @@
+import requests
+import json 
+
+class GoogleSearch:
+    def __init__(self, logger, api_key, cse_id):
+        self.__logger = logger
+        self.api_key = api_key
+        self.cse_id = cse_id
+
+    def google_search(self, search_term, start_page):
+        self.__logger.info(f"searching for term {search_term} in page {start_page}")
+        search_url = "https://www.googleapis.com/customsearch/v1"
+        params = {
+            'q': search_term,
+            'cx': self.cse_id,
+            'key': self.api_key,
+            'start': start_page
+        }
+        response = requests.get(search_url, params=params)
+        self.__logger.info(f"Google response status {response.status_code}")
+        if response.status_code == 200:
+            result = response.json()
+            return result
+
+    def get_urls(self, data):
+        url_list = []
+        for item in data['items']:
+            if item.get('link'):
+                url_list.append(item['link'])
+        return url_list
+
+    def fetch_urls(self, search_term, start_page):
+        results = self.google_search(search_term, start_page)
+        return self.get_urls(results)
